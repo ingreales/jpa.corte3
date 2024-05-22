c4a35e5d13f0a611b4940c02eab90a6b712846f0
Juan Diego Reales Garcia <132092106+ingreales@users.noreply.github.com>
2024-05-21 19:54:23 -0500

    Add files via upload
    juan Diego Reales García, Andrea Jaramillo

---
+++ JPA.corte3/src/main/java/org/example/Hibernate_Id.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import org.example.utilidades.JpaUtil;
+
+import java.util.Scanner;
+
+public class Hibernate_Id {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        System.out.println("Ingrese el id: ");
+        Long id = s.nextLong();
+        EntityManager em = JpaUtil.getEntityManager();
+        Clientes cliente = em.find(Clientes.class, id);
+        System.out.println(cliente);
+        Clientes cliente2 = em.find(Clientes.class, id);
+        System.out.println(cliente2);
+        em.close();
+    }
+
+}
